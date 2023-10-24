69f88547512b6e022fabd464197e779e9c95d8c8
skattagmail <srinivasakatta@gmail.com>
2023-10-24 10:26:43 +0530

    backend file modified

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "myterraform-state-bucketjenkins"
     key = "main"
-    region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    region = "us-east-2"
+    dynamodb_table = "mydynamodb-table"
   }
 }
